c184ba91bc8ef883e6c7fa3a1c9516056d59e0a5
Andriy Mykhaylyk <erp.lsf@gmail.com>
2023-05-01 17:48:01 +0200

    add nix stuff

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ pkgs ? (let
+  inherit (builtins) fetchTree fromJSON readFile;
+  inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+in import (fetchTree nixpkgs.locked) {
+  overlays = [ (import "${fetchTree gomod2nix.locked}/overlay.nix") ];
+}) }:
+
+pkgs.buildGoApplication {
+  pname = "aws-sso-cli";
+  version = "1.9.10";
+  pwd = ./.;
+  src = ./.;
+  modules = ./gomod2nix.toml;
+}
