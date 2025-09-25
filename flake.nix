eb93335a9806e26d6ce82c30ef1cdcb8bdb876a3
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-09-25 12:14:54 +0200

    update vendor hash

--- flake.nix
+++ flake.nix
@@ -4,20 +4,29 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-      in {
+      in
+      {
         packages.default = pkgs.buildGoModule {
           pname = "aws-sso";
           version = "main";
 
           src = ./.;
 
-          vendorHash = "sha256-SNMU7qDfLRGUSLjzrJHtIMgbcRc2DxXwWEUaUEY6PME=";
+          vendorHash = "sha256-K4VdK4E+ShQtkr+CwN2K0qPuL0wfsMFW1pLU/JSslxs=";
 
           checkFlags = [ "-skip=^TestDetectShellBash$" ];
         };
-      }));
+      }
+    ));
 }
